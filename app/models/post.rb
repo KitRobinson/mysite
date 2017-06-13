4d6e89a1ffbd3ee9c79d3353d112e79beb1fef56
Kit Robinson <krobinson.dev@gmail.com>
2017-06-13 12:21:41 -0400

    add model associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
 	mount_uploader :image_url, ImageUploader
+
+	has_many :comments
+	has_many :posts_tags
+	has_many :tags, through: :posts_tags
+	
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,6 @@
 class Tag < ActiveRecord::Base
+
+	has_many :posts_tags
+	has_many :posts, through: :posts_tags
+	
 end
